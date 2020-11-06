2bf1eb0b164830e40e5007b2248b27bcc5ca4536
BrendonLH <Brendonhampton1988@gmail.com>
2020-11-05 18:04:44 -0800

    finished adding dependancies and installing superagent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      radio: "",
+      textArea: "",
+      variable: ""
+    };
+  }
+
+  handleEventChange = (event) => {
+    const restMethod = event.target.name;
+    const requestValue = event.target.value;
+    console.log(restMethod, requestValue);
+    this.state = this.setState({ [restMethod]: requestValue });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    const textArea = `${this.state.radio} ${this.state.variable}`;
+    this.state = this.setState({textArea});
+    
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>RESTy</h1>
+        <nav>
+          <a href="#"><li>Home</li></a>
+          <a href="#"><li>History</li></a>
+          <a href="#"><li>Help</li></a>
+        </nav>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+          value={this.state.variable}
+          name="variable"
+          type="url"
+          placeholder="http://www.url.com"
+          onChange={this.handleEventChange}
+          />
+          <button type="submit">Submit</button>
+          <br />
+          <div className="methodButtons">
+            <input
+              type="radio"
+              name="radio"
+              value="get"
+              onChange={this.handleEventChange}
+            />
+            <label>GET</label>
+            <input
+              type="radio"
+              name="radio"
+              value="post"
+              onChange={this.handleEventChange}
+            />
+            <label>POST</label>
+            <input
+              type="radio"
+              name="radio"
+              value="put"
+              onChange={this.handleEventChange}
+            />
+            <label>PUT</label>
+            <input
+              type="radio"
+              name="radio"
+              value="delete"
+              onChange={this.handleEventChange}
+            />
+            <label>DELETE</label>
+          </div>
+          <br />
+          <br />
+
+          <textarea value={this.state.textArea}>
+            
+          </textarea>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
